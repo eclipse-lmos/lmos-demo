18a097eacc4a1e8e7b45a180f525ca4bd9810e0f
Henning Sudbrock <henning.sudbrock@telekom.de>
2025-03-10 11:47:35 +0100

    Switch to graphql endpoint of lmos-runtime, and switch to arc-view version from eclipse-lmos (#42)
    * Switch to graphql endpoint of lmos-runtime, and switch to arc-view version from eclipse-lmos
    
    The previously used version of the arc-view was from a some months old fork that enabled the arc-view to talk to the lmos-runtime using REST; with this commit this is no longer needed, as the lmos-runtime can by now also speak graphql.
    
    * Use hosted version of the arc-view for now, because the "agentUrl" query parameter appears not yet to be supported in the arc-view running inside the demo

--- install.sh
+++ install.sh
@@ -28,8 +28,9 @@ kubectl delete secret lmos-runtime 2> /dev/null
 kubectl create secret generic lmos-runtime --from-literal=OPENAI_API_KEY="$OPENAI_APIKEY"
 
 # Install lmos-runtime chart
-helm upgrade --install lmos-runtime oci://ghcr.io/eclipse-lmos/lmos-runtime-chart \
+helm upgrade --install lmos-runtime oci://ghcr.io/eclipse-lmos/lmos-runtime-graphql-chart \
  --version 0.1.0-SNAPSHOT \
+ --set openaiApiProvider="$OPENAI_PROVIDER" \
  --set openaiApiUrl="$OPENAI_URL" \
  --set openaiApiModel="$OPENAI_MODELNAME" \
  --set agentRegistryUrl=http://lmos-operator.default.svc.cluster.local:8080 \
@@ -42,7 +43,7 @@ while ! kubectl get crd agents.lmos.eclipse >/dev/null 2>/dev/null; do sleep 1; 
 echo "LMOS agent CRD created."
 
 # Install arc-view chart
-helm upgrade --install arc-view-runtime-web oci://ghcr.io/lmos-ai/arc-view-runtime-web-chart --version 0.1.0
+helm upgrade --install arc-view-web oci://ghcr.io/eclipse-lmos/arc-view-web-chart --version 0.1.0-SNAPSHOT
 
 # Install dashboard for the lmos-demo
 helm upgrade --install dashboard-lmos-demo ./dashboard/helm-chart
@@ -53,7 +54,7 @@ while ! kubectl get pods -n istio-system | grep kiali | grep -q Running; do slee
 while ! kubectl get pods -n istio-system | grep grafana | grep -q Running; do sleep 1; done
 while ! kubectl get pods -n istio-system | grep prometheus | grep -q Running; do sleep 1; done
 while ! kubectl get pods | grep lmos-runtime | grep -q Running; do sleep 1; done
-while ! kubectl get pods | grep arc-view-runtime-web | grep -q Running; do sleep 1; done
+while ! kubectl get pods | grep arc-view-web | grep -q Running; do sleep 1; done
 while ! kubectl get pods | grep dashboard-lmos-demo | grep -q Running; do sleep 1; done
 
 # Set up port forwarding
@@ -62,7 +63,7 @@ nohup kubectl -n istio-system port-forward svc/kiali 20001:20001 >/dev/null 2>&1
 nohup kubectl -n istio-system port-forward svc/grafana 3000:3000 >/dev/null 2>&1 &
 nohup kubectl -n istio-system port-forward svc/prometheus 9090:9090 >/dev/null 2>&1 &
 nohup kubectl port-forward svc/lmos-runtime 8081:8081 >/dev/null 2>&1 &
-nohup kubectl port-forward svc/arc-view-runtime-web-service 8080:80 >/dev/null 2>&1 &
+nohup kubectl port-forward svc/arc-view-web 8080:80 >/dev/null 2>&1 &
 nohup kubectl port-forward svc/dashboard-lmos-demo 8082:8080 >/dev/null 2>&1 &
 
 # Route 100% of traffic to stable channels
